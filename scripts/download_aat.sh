ebcb0e79e7205371fb09dff65397d445d9579523
andrecastro0o <andre.castro@dans.knaw.nl>
2024-12-05 15:12:38 +0100

    SSHOCNL-5  download n unzip

--- scripts/download_aat.sh
+++ scripts/download_aat.sh
@@ -2,5 +2,6 @@
 
 # download only explicit statements
 wget http://aatdownloads.getty.edu/VocabData/explicit.zip
-unzip explicit 
+mkdir aat/
+unzip explicit.zip -d aat/ 
 # for explicit+inferred terms use  http://aatdownloads.getty.edu/VocabData/full.zip
