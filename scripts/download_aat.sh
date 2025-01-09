c2e353c455120b214fb3fe391a9e25ee3506f6b3
andrecastro0o <andre.castro@dans.knaw.nl>
2025-01-09 17:01:10 +0100

    deleted scripts/download_aat.sh

--- scripts/download_aat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# download only explicit statements
-wget http://aatdownloads.getty.edu/VocabData/explicit.zip
-mkdir aat/
-unzip explicit.zip -d aat/ 
-# for explicit+inferred terms use  http://aatdownloads.getty.edu/VocabData/full.zip
